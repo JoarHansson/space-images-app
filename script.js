1b1e4a3a43a0b26e19c4f24219c1e382d13dc075
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2023-12-03 12:54:03 +0100

    Add basic styling, add timeout for error message

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const baseUrl =
   "https://api.nasa.gov/planetary/apod?api_key=v3uFn7uhfV3YcTTafG142dqcDpC1FClGV4xWMmeM";
 
-const buttonGetRandomImage = document.querySelector("#button-random-image");
-
+const buttonRandomImage = document.querySelector("#button-random-image");
 const dateInput = document.querySelector("#input-date");
-
-const container = document.querySelector("#container-image");
+const containerImage = document.querySelector("#container-image");
+const containerInfo = document.querySelector("#container-info");
 
 // fetch data, customize output by modifying url parameter
 async function fetchData(url) {
@@ -15,8 +14,8 @@ async function fetchData(url) {
 
     console.log("1", response.status);
 
-    while (container.hasChildNodes()) {
-      container.removeChild(container.firstChild);
+    while (containerImage.hasChildNodes()) {
+      containerImage.removeChild(containerImage.firstChild);
     }
 
     console.log("2", data);
@@ -24,26 +23,38 @@ async function fetchData(url) {
     if (response.status != 200) {
       const errorMessage = document.createElement("div");
       errorMessage.textContent = data.msg; // the error message provided in the object
-      container.appendChild(errorMessage);
+      containerImage.appendChild(errorMessage);
     } else {
       // data comes as an object or as an array (each index containing an object)
-      let dataFormat;
+      let dataChecked;
       if (Array.isArray(data)) {
-        dataFormat = data[0];
+        dataChecked = data[0];
       } else {
-        dataFormat = data;
+        dataChecked = data;
       }
 
       // append image, skip videos.
-      if (dataFormat.media_type === "image") {
+      if (dataChecked.media_type === "image") {
         const image = document.createElement("img");
-        image.src = dataFormat.url;
-        container.appendChild(image);
-      } else if (dataFormat.media_type === "video") {
-        const errorVideo = document.createElement("div");
-        errorVideo.textContent =
-          "No image available on this day. Choose another day.";
-        container.appendChild(errorVideo);
+        image.src = dataChecked.url;
+        containerImage.appendChild(image);
+      } else if (dataChecked.media_type === "video") {
+        const errorMessageVideo = document.createElement("div");
+
+        // display error message for 5 sec, then get a new random image:
+        let i = 5;
+        let interval = setInterval(() => {
+          errorMessageVideo.textContent = `No image available on this day. Getting a random one in ${i}`;
+          i--;
+        }, 1000);
+
+        containerImage.appendChild(errorMessageVideo);
+
+        setTimeout(() => {
+          // get a random image
+          fetchData(baseUrl + "&count=1");
+          clearInterval(interval);
+        }, 6000);
       }
     }
   } catch (error) {
@@ -58,6 +69,19 @@ dateInput.addEventListener("change", (event) => {
 });
 
 // get a random image
-buttonGetRandomImage.addEventListener("click", () => {
+buttonRandomImage.addEventListener("click", () => {
   fetchData(baseUrl + "&count=1");
 });
+
+// make sure large images scales down to fit between header and footer
+function setElementHeight() {
+  const headerHeight = document.querySelector("header").offsetHeight;
+  const footerHeight = document.querySelector("footer").offsetHeight;
+
+  containerImage.style.height = `calc(100% - ${headerHeight}px - ${footerHeight}px)`;
+}
+
+window.addEventListener("resize", setElementHeight);
+
+// Call the function initially
+setElementHeight();
