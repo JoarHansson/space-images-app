6c71d6953a72939d46062ee1a1c5f2a72d087f13
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-04-29 14:07:58 +0200

    change folder structure

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
-import legacy from "@vitejs/plugin-legacy";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
-});
+  plugins: [react()],
+})
