3762a93cf5e628094cd7591ed9676dea590ea131
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-02 15:44:05 +0200

    add close button icon

--- src/components/InfoPopup/InfoPopup.jsx
+++ src/components/InfoPopup/InfoPopup.jsx
@@ -1,10 +1,11 @@
+import { X } from "lucide-react";
 import styles from "./InfoPopup.module.css";
 
 function InfoPopup({ fetchedData, onClickCloseButton }) {
   return (
     <div className={styles.containerInfo}>
       <button className={styles.closeButton} onClick={onClickCloseButton}>
-        X
+        <X strokeWidth={3} />
       </button>
       <p className={styles.imageTitle}>Title: {fetchedData.title}</p>
       <p className={styles.imageDate}>Date: {fetchedData.date}</p>
