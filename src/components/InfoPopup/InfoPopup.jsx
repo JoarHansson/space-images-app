a3fdfea002b60bbc7368cd1f1aaec5900d29c635
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-12 13:37:39 +0200

    change some styiling

--- src/components/InfoPopup/InfoPopup.jsx
+++ src/components/InfoPopup/InfoPopup.jsx
@@ -3,18 +3,20 @@ import styles from "./InfoPopup.module.css";
 
 function InfoPopup({ fetchedData, onClickCloseButton }) {
   return (
-    <div className={styles.containerInfo}>
-      <button className={styles.closeButton} onClick={onClickCloseButton}>
-        <X strokeWidth={3} />
-      </button>
-      <p className={styles.imageTitle}>Title: {fetchedData.title}</p>
-      <p className={styles.imageDate}>Date: {fetchedData.date}</p>
-      <p className={styles.imageCopyright}>
-        Copyright: {fetchedData.copyright ?? "Not specified"}
-      </p>
-      <p className={styles.imageExplanation}>
-        Explanation: {fetchedData.explanation}
-      </p>
+    <div className={styles.containerBlurred}>
+      <div className={styles.containerInfo}>
+        <button className={styles.closeButton} onClick={onClickCloseButton}>
+          <X strokeWidth={3} />
+        </button>
+        <p className={styles.imageTitle}>Title: {fetchedData.title}</p>
+        <p className={styles.imageDate}>Date: {fetchedData.date}</p>
+        <p className={styles.imageCopyright}>
+          Copyright: {fetchedData.copyright ?? "Not specified"}
+        </p>
+        <p className={styles.imageExplanation}>
+          Explanation: {fetchedData.explanation}
+        </p>
+      </div>
     </div>
   );
 }
