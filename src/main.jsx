07735f475d03f908434f4ada5f734dcb334cf296
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-15 13:35:47 +0200

    make another try to fix routing bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // packages:
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages:
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
