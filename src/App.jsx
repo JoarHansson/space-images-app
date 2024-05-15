6fe7b457942ff33f1191ba39ad2b7e943e0f7451
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-15 13:47:12 +0200

    make another try to fix routing bug
    clarification:
    when deployed, refreshing on any other page than "/", returns 404.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 // packages:
 import { AnimatePresence } from "framer-motion";
-import { useLocation, useRoutes } from "react-router-dom";
+import { useLocation, useRoutes, Routes, Route } from "react-router-dom";
 // pages:
 import Index from "./pages/Index";
 import Home from "./pages/Home";
 
 export default function App() {
-  const element = useRoutes([
-    {
-      path: "/",
-      element: <Index />,
-    },
-    {
-      path: "/home",
-      element: <Home />,
-    },
-  ]);
-
   const location = useLocation();
-
-  if (!element) return null;
 
   return (
     <AnimatePresence mode="wait" initial={false}>
-      {React.cloneElement(element, { key: location.pathname })}
+      <Routes location={location} key={location.pathname}>
+        {/* Routes */}
+        <Route index element={<Index />} />
+        <Route path="home" element={<Home />} />
+
+        {/* 404 page */}
+        <Route
+          path="*"
+          element={<div style={{ margin: "2rem" }}>404 - not found</div>}
+        />
+      </Routes>
     </AnimatePresence>
   );
 }
