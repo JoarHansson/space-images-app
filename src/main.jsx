e04536ddceb310244ec19334c88e967f2221941a
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-15 12:29:23 +0200

    try something else to fix routing bug

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
