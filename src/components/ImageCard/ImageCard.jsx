5ccaf2209b807d8b2071d537c4f1173fcdc913b9
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-05-29 15:17:29 +0200

    fix bug with overflowing images
    however, the ImageCard component now only works on the Home page, not on the Likes page.
    Probably ImageCard will have to be split into two versions.

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 // components:
 import HeartButton from "../../components/HeartButton/HeartButton";
 import InfoPopup from "../../components/InfoPopup/InfoPopup";
@@ -7,20 +7,52 @@ import styles from "./ImageCard.module.css";
 
 const ImageCard = ({ imageData, imgLiked, imgLoaded, onImgLoad, onClick }) => {
   const [containerInfoVisible, setContainerInfoVisible] = useState(false);
+  const imgRef = useRef(null);
+  const containerFooterRef = useRef(null);
+
+  const [containerHeight, setContainerHeight] = useState();
+  const [containerWidth, setContainerWidth] = useState();
+
+  function getImageHeight() {
+    const imgHeight = imgRef.current.offsetHeight;
+    const imgWidth = imgRef.current.offsetWidth;
+
+    setContainerHeight(imgHeight);
+    setContainerWidth(imgWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", getImageHeight);
+
+    // Set image height initially:
+    getImageHeight();
+
+    // Clean up function
+    return () => {
+      window.removeEventListener("resize", getImageHeight);
+    };
+  }, [onImgLoad]);
 
   return (
     <>
-      <div
-        className={styles.container}
-        key={imageData.url}
+      <img
+        className={styles.image}
+        src={imageData.url}
+        alt={imageData.title}
+        onLoad={onImgLoad}
+        onResize={getImageHeight}
+        ref={imgRef}
         style={imgLoaded ? {} : { display: "none" }}
+      />
+      <div
+        ref={containerFooterRef}
+        className={styles.container}
+        style={{
+          height: containerHeight,
+          width: containerWidth,
+          ...(imgLoaded ? {} : { display: "none" }),
+        }}
       >
-        <img
-          className={styles.image}
-          src={imageData.url}
-          alt={imageData.title}
-          onLoad={onImgLoad}
-        />
         <div className={styles.imageFooter}>
           {/* button to add or remove like of the photo: */}
           <HeartButton onClick={onClick} imgLiked={imgLiked} />
